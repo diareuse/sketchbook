57ce8b6df89a7478d3045fdeeacb535cbd5a7677
Viktor De Pasquale <hello@depasquale.wiki>
2024-01-28 21:11:56 +0100

    Added rocker counter

---
+++ core/src/main/java/lib/sketchbook/SketchbookRockerCounter.kt
@@ -0,0 +1,62 @@
+@file:OptIn(ExperimentalMaterial3Api::class)
+
+package lib.sketchbook
+
+import android.content.res.Configuration.UI_MODE_NIGHT_NO
+import android.content.res.Configuration.UI_MODE_NIGHT_YES
+import androidx.compose.foundation.background
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Favorite
+import androidx.compose.material.icons.filled.FavoriteBorder
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.ProvideTextStyle
+import androidx.compose.material3.SingleChoiceSegmentedButtonRow
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import lib.sketchbook.theme.Theme
+
+@Composable
+fun SketchbookRockerCounter(
+    decrement: @Composable () -> Unit,
+    increment: @Composable () -> Unit,
+    label: @Composable () -> Unit,
+    modifier: Modifier = Modifier
+) {
+    SingleChoiceSegmentedButtonRow(
+        modifier = modifier
+            .background(Theme.color.overlay.background.copy(.05f), Theme.container.button)
+            .clip(Theme.container.button),
+        space = 0.dp
+    ) {
+        decrement()
+        ProvideTextStyle(
+            Theme.textStyle.body.copy(
+                fontWeight = FontWeight.Bold,
+                textAlign = TextAlign.Center
+            )
+        ) {
+            label()
+        }
+        increment()
+    }
+}
+
+@Preview(showBackground = true, uiMode = UI_MODE_NIGHT_YES)
+@Preview(showBackground = true, uiMode = UI_MODE_NIGHT_NO)
+@Composable
+private fun SketchbookRockerCounterPreview(
+) = SketchbookPreviewLayout {
+    SketchbookRockerCounter(
+        decrement = { IconButton({}) { Icon(Icons.Default.FavoriteBorder, null) } },
+        increment = { IconButton({}) { Icon(Icons.Default.Favorite, null) } },
+        label = { Text("3") }
+    )
+}
